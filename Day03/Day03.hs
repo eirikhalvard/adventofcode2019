4864aad72bd4cb013011b2f4fa4eb8b0c4be0b82
Eirik Halvard Sæther <eirik.halvard.95@gmail.com>
2019-12-03 23:05:14 +0100

    finished 3b

--- Day03/Day03.hs
+++ Day03/Day03.hs
@@ -6,10 +6,17 @@ import Text.Parsec.Char (newline, string, digit)
 import Control.Applicative
 import Control.Monad
 import qualified Data.Set as S
+import qualified Data.Map.Lazy as M
 
 data Direction = L | R | U | D deriving (Show)
 type WirePart = (Direction, Int)
 type Wire = [WirePart]
+
+updateCoords :: (Int, Int) -> Direction -> (Int, Int)
+updateCoords (x,y) L = (x-1,y)
+updateCoords (x,y) R = (x+1,y)
+updateCoords (x,y) U = (x,y+1)
+updateCoords (x,y) D = (x,y-1)
 
 solveA :: (Wire, Wire) -> Int
 solveA (a,b) = minimum
@@ -19,10 +26,6 @@ solveA (a,b) = minimum
                  (walk (0,0) S.empty a)
                  (walk (0,0) S.empty b)
   where
-    updateCoords (x,y) L = (x-1,y)
-    updateCoords (x,y) R = (x+1,y)
-    updateCoords (x,y) U = (x,y+1)
-    updateCoords (x,y) D = (x,y-1)
     walk coords s []            = S.insert coords s
     walk coords s ((dir, 0):ws) = walk coords s ws
     walk coords s ((dir, n):ws) = 
@@ -30,8 +33,23 @@ solveA (a,b) = minimum
     manhattan = uncurry (+) . (each %~ abs)
     
 
--- solveB :: _ -> _
-solveB = undefined
+solveB :: (Wire, Wire) -> Int
+solveB (a,b) = minimum
+             $ M.delete (0,0)
+             $ M.intersectionWith (+)
+                 (walk 0 (0,0) M.empty a)
+                 (walk 0 (0,0) M.empty b)
+  where
+    walk steps coords m []
+      | M.member coords m = m
+      | otherwise = M.insert coords steps m
+    walk steps coords m ((dir, 0):ws) = walk steps coords m ws
+    walk steps coords m ((dir, n):ws)
+      | M.member coords m = walk (steps+1) (updateCoords coords dir) 
+          m ((dir, n-1):ws)
+      | otherwise = walk (steps+1) (updateCoords coords dir) 
+          (M.insert coords steps m) ((dir, n-1):ws)
+    
 
 parseInput :: String -> (Wire, Wire)
 parseInput xs = 
@@ -54,7 +72,5 @@ parseInput xs =
 main :: IO ()
 main = do
   wires <- parseInput <$> readFile "input.txt"
-  print $ solveA $ parseInput "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83\n"
   print $ solveA wires
-  -- print $ solveB wires
-  return ()
+  print $ solveB wires
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# adventofcode2019
+# Advent of Code 2019
+
+By Eirik Halvard Sæther
+
 My Haskell solutions for advent of code 2019
