9672a8dd20784e3373fdee2a94501cc089208b41
Eirik Halvard Sæther <eirik.halvard.95@gmail.com>
2019-12-10 16:42:58 +0100

    fixed day10 b. escaped from hell

--- Day10/Day10.hs
+++ Day10/Day10.hs
@@ -1,7 +1,8 @@
+{-# LANGUAGE TupleSections #-}
 module Day10 where
 
 import Control.Lens
-import Data.List.Extra (nubOn)
+import Data.List.Extra (nubOn, minimumOn, maximumOn, groupOn, sortOn)
 import Data.Tuple (swap)
 
 type Asteroid = (Int,Int)
@@ -13,16 +14,36 @@ parseInput = toListOf ((itraversed<.>itraversed)
                       . to (swap . fst))
            . lines
 
-solveA :: [Asteroid] -> Int
-solveA positions = maximum (fmap numInSight positions)
+solveA :: [Asteroid] -> (Int, Asteroid)
+solveA positions = maximumOn fst (fmap numInSight positions)
   where
-    numInSight pos = length $ nubOn (getAngleFrom pos) positions
-    getAngleFrom a b = uncurry atan2 . (each %~ fromIntegral) $ coordDiff a b
-    coordDiff (y,x) (y',x') = (y-y', x-x')
-                          
+    numInSight pos = (,pos) . length $ nubOn (angleFrom pos) positions
+
+solveB :: Asteroid -> [Asteroid] -> Int
+solveB pos positions = asteroidX * 100 + asteroidY
+  where 
+    (asteroidX, asteroidY) = minimumOn (lengthFrom pos) . (!! 199) 
+                           . fmap (sortOn (lengthFrom pos)) 
+                           . groupOn f 
+                           . sortOn f 
+                           . filter (/= pos) 
+                           $ positions
+    f = angleFromTop pos
+    lengthFrom (x,y) (x',y') = (y-y')^2 + (x-x')^2
+
+
+angleFromTop :: Asteroid -> Asteroid -> Float
+angleFromTop (x,y) (x',y') = pi - atan2 (fromIntegral (x'-x)) (fromIntegral (y'-y))
+
+angleFrom :: Asteroid -> Asteroid -> Float
+angleFrom a b = uncurry atan2 . (each %~ fromIntegral) $ coordDiff a b
+  where coordDiff (y,x) (y',x') = (y'-y, x'-x)
 
 
 main :: IO ()
 main = do
   xs <- readFile "input.txt"
-  print . solveA . parseInput $ xs
+  let asteroids = parseInput xs
+      (amount, largest) = solveA asteroids
+  putStrLn ("Amount: " ++ show amount ++ ", Largest: " ++ show largest)
+  print $ solveB largest asteroids
